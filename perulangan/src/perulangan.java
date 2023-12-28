d79b1a9ee992923e162c6298668fcb55eb048779
ardila <146051568+putuardila@users.noreply.github.com>
2023-12-28 12:45:48 +0800

    LKM_03_PERULANGAN_28-12-2023

---
+++ perulangan/src/perulangan.java
@@ -0,0 +1,14 @@
+public class perulangan {
+    public static void  main(String[] args) {
+
+        // counted looping For
+        for(int i=0; i < 5 ; i++){
+            System.out.println("ke-" ++ "***** ");
+        }
+    }
+
+}
+    
+
+
+    
